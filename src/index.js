43f68c8d6ed702f6e313046954a01fe325af36a3
Nastya Kladova <nstsologub@gmail.com>
2022-01-04 11:41:26 +0300

    feat: add the decision of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    //   let newNum = Math.abs(n);
+    let numCheck = Math.abs(n);
+    let str = numCheck.toString();
+    let newNum;
+
+    if (str.endsWith('000')) {
+        newNum = str.slice(0, -3).split('').reverse().join('');
+    } else if (str.endsWith('00')) {
+        newNum = str.slice(0, -2).split('').reverse().join('');
+    } else if (str.endsWith('0')) {
+        newNum = str.slice(0, -1).split('').reverse().join('');
+    } else {
+        newNum = str.split('').reverse().join('');
+    }
+
+    return newNum;
 }
+
